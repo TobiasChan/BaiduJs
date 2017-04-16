895950895e878a13e5ad81e5d70460c62ce814ef
TobiasChan <471830980@qq.com>
2017-04-16 14:48:58 +0800

    Btask4

---
+++ Btask4/Btask4.js
@@ -0,0 +1,81 @@
+//事件绑定函数，兼容浏览器差异
+function addEvent(element, event, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(event, listener, false);
+    }
+    else if (element.attachEvent) {
+        element.attachEvent("on"+event, listener);
+    }
+    else {
+        element["on"+event]=listener;
+    }
+}
+
+//绑定事件
+var order=document.getElementById('order');
+var act=document.getElementById('action');
+var mybox=document.getElementById('myBox');
+var btn=document.getElementsByClassName('button');
+var angle=0;//角度计数
+var count=1;//计算步数
+
+//判断函数
+function takeOrder(e) {
+    if(e.value=="TUN LEF"){
+        //为了不出现负方向先进行判断
+        if(count==1) {
+        	count=4;
+        } else {
+        	count=(count-1)%4;
+        }
+        mybox.style.transform="rotate("+(angle-90)+"deg)";
+        angle=-90+angle;
+        console.log(count);
+    }
+    if(e.value=="TUN RIG"){
+        if(count==3) {
+        	count=4;
+        } else {
+        	count=(count+1)%4;
+        }
+        mybox.style.transform="rotate("+(angle+90)+"deg)";
+        angle=90+angle;
+        console.log(count);
+    }
+    if(e.value=="TUN BAC"){
+        if(count==2) {
+        	count=4;
+        } else {
+        	count=(count+2)%4;
+        }
+        mybox.style.transform="rotate("+(angle+180)+"deg)";
+        angle=180+angle;
+        console.log(count);
+    }
+    if(e.value=="GO"){
+        if(count==1) {
+        	mybox.style.top=="0px" ? alert("出界") : mybox.style.top=(parseInt(mybox.style.top)-40)+"px"
+        };
+        if(count==2) {
+        	mybox.style.left=="360px" ? alert("出界") : mybox.style.left=(parseInt(mybox.style.left)+40)+"px"
+        };
+        if(count==3) {
+        	mybox.style.top=="360px" ? alert("出界") : mybox.style.top=(parseInt(mybox.style.top)+40)+"px"
+        };
+        if(count==4) {
+        	mybox.style.left=="0px" ? alert("出界") : mybox.style.left=(parseInt(mybox.style.left)-40)+"px"
+        };
+	}
+}
+
+//调用函数
+addEvent(act,"click",function(){
+	takeOrder(order);
+});
+
+//附加4个按钮
+for (let i=0; i<btn.length; i++) {
+	addEvent(btn[i],"click",function(){
+		takeOrder(btn[i]);
+	});
+}
